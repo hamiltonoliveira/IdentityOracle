b374111676aefeb49cab8fea02948d62f66afa86
Thiago Borges Vieira <thiago@upgradesolutions.com.br>
2016-08-02 09:06:28 -0300

    Projeto Identity Oracle

---
+++ src/TB.IdentityOracle.Presentation.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TB.IdentityOracle.Presentation.MVC.MvcApplication" Language="C#" %>
---
+++ src/TB.IdentityOracle.Presentation.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TB.IdentityOracle.Presentation.MVC.Startup))]
+namespace TB.IdentityOracle.Presentation.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
